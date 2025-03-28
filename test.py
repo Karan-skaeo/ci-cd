766e4062fee459bee3b1f2896b223b0ee58252a4
Karan saini <karansaini@Karans-MacBook-Air.local>
2025-03-28 11:42:21 +0530

    testing env print

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-print("hello world")
+import os
+import dotenv
+
+dotenv.load_dotenv()
+app1 = os.environ.get('APP_USERNAME_OR_EMAIL')
+pswrd = os.environ.get('APP_PASSWORD')
+jwt = os.environ.get('JWT_SECRET_KEY')
+print(jwt, app1,pswrd)
